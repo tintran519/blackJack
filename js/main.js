024d5a5bde27e9f62e1c680a1da8488776f5f440
Tin Tran <tin.tran0519@gmail.com>
2016-10-10 22:00:15 -0700

    updated user interface and game logic

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ var playerHand = [];
 var dealerHand = [];
 var playerCurrentscore = 0;
 var dealerCurrentscore = 0;
+var playerMoney = 0;
+var moneyPool = 0;
+
+// this.htmlE = $('<divx
 
 //TALLYING SCORE (add values in array at all times)
 //Player's score
@@ -15,6 +19,8 @@ function playerScore() {
       playerSum -= 10 * aceCounter();
     }
       playerCurrentscore = playerSum;
+
+      $('#pScore').text(playerCurrentscore);
 
       if(playerCurrentscore >= 21) {
         result();
@@ -36,47 +42,55 @@ function dealerScore() {
 //Creating the deck
 var deck =[];
 
-function createDeck() {
-for(var i=0; i < 52; i++) {
-var suits = '';
-var face = String(i % 13);
-var value = i % 13;
-var code = i % 13;
-
-var Card = function(suits,face,code,value) {
+var Card = function(suits,face,code,value,className) {
     this.suits = suits;
     this.face = face;
     this.code = code;
     this.value = value;
+    this.className = className;
 }
 
-if(i <= 13) {
-  suits = 'hearts';
-} else if(i > 13 && i <= 26) {
-  suits = 'spades';
-} else if (i >26 && i <= 39) {
-  suits = 'diamonds';
-} else if (i >39 && i <=52) {
-  suits = 'clover';
-}
+function createDeck() {
+    for(var i=0; i < 52; i++) {
+    var suits = '';
+    var face = String(i % 13);
+    var value = i % 13;
+    var code = i % 13;
 
-if (code === 0) {
-    face = 'King';
-    value = 10
-  } else if (code === 11) {
-    face = 'Jack';
-    value = 10;
-  } else if (code === 12) {
-    face = 'Queen';
-    value = 10;
-  } else if (code === 1) {
-    face = 'Ace';
-    value = 11;
-  }
 
-var card = new Card(suits,face,code,value);
+    if(i <= 13) {
+      suits = 'hearts';
+    } else if(i > 13 && i <= 26) {
+      suits = 'spades';
+    } else if (i >26 && i <= 39) {
+      suits = 'diamonds';
+    } else if (i >39 && i <=52) {
+      suits = 'clubs';
+    }
 
-deck.push(card);
+    if (code === 0) {
+        face = 'K';
+        value = 10
+      } else if (code === 11) {
+        face = 'J';
+        value = 10;
+      } else if (code === 12) {
+        face = 'Q';
+        value = 10;
+      } else if (code === 1) {
+        face = 'A';
+        value = 11;
+      }
+
+    if (value < 10) {
+      className = 'card ' + suits[0] + '0' + value;
+    } else {
+      className = 'card ' + suits[0] + face;
+    }
+
+    var card = new Card(suits,face,code,value,className);
+
+    deck.push(card);
   }
 }
 
@@ -92,8 +106,15 @@ playerCurrentscore = 0;
 dealerCurrentscore = 0;
 playerHand.push(deck.pop(),deck.pop());
 dealerHand.push(deck.pop(),deck.pop());
+$('#dScore').text('?');
+$('#dealer').empty();
+$('#dealer').append('<div class="card back-blue"></div>');
+$('#dealer').append('<div class="' + dealerHand[1].className + '"></div>');
 playerScore();
 dealerScore();
+viewPlayerHand();
+$('#hit').click(playerHit);
+$('#stay').click(result);
 };
 
 //Player Stays scenario
@@ -102,7 +123,7 @@ function result() {
 if((playerCurrentscore === 21 && dealerCurrentscore === 21) || (playerCurrentscore === dealerCurrentscore)) {
   alert("Tie Game!");
   } else if (playerCurrentscore > 21) {
-    alert("Bust...");
+  alert("Bust...");
   } else if (playerCurrentscore === 21) {
   alert("You got Blackjack!");
   } else if (dealerCurrentscore === 21) {
@@ -114,13 +135,15 @@ if((playerCurrentscore === 21 && dealerCurrentscore === 21) || (playerCurrentsco
   } else {
   alert("You won!");
   }
+  $('#hit').off();
+  $('#stay').off();
 };
 
 //Player Ace Logic
 function aceCounter() {
 var numAce = 0;
 for(var i = 0; i < playerHand.length; i++) {
-if(playerHand[i].face === "Ace") {
+if(playerHand[i].face === "A") {
 numAce += 1;
 }
 }
@@ -129,13 +152,13 @@ return numAce;
 
 //Dealer Ace Logic
 function aceCounterDealer() {
-var numAceDealer = 0;
-for(var i = 0; i < dealerHand.length; i++) {
-if(dealerHand[i].face === "Ace") {
-numAceDealer += 1;
-}
-}
-return numAceDealer;
+  var numAceDealer = 0;
+  for(var i = 0; i < dealerHand.length; i++) {
+    if(dealerHand[i].face === "A") {
+      numAceDealer += 1;
+    }
+  }
+  return numAceDealer;
 }
 
 //Hit
@@ -143,6 +166,7 @@ function playerHit() {
 
   if(playerCurrentscore < 21) {
     playerHand.push(deck.pop());
+    viewPlayerHand();
   }
     playerScore();
 };
@@ -154,8 +178,40 @@ function playerStay() {
     dealerHand.push(deck.pop());
     dealerScore();
   }
+  viewPlayerHand();
+  viewDealerHand();
+  $('#dScore').text(dealerCurrentscore);
 }
 
-//Fix double bust...
+//Render display/////////////
+//PlayerHand
+function viewPlayerHand() {
+  $('#player').empty();
+  $.each(playerHand, function(index,value) {
+    $('#player').append('<div class="' + value.className + '"></div>');
+  }
+  )};
 
+//DealerHand
+function viewDealerHand() {
+  $('#dealer').empty();
+  $.each(dealerHand, function(index,value) {
+    $('#dealer').append('<div class="' + value.className + '"></div>');
+  }
+  )};
+
+//Stay
+$('#deal').click(startGame);
+
+//Hit
+$('#hit').click(playerHit);
+
+//Stay
+$('#stay').click(result);
+
+//Money/////////////////
+function bet25() {
+  playerMoney -= 25;
+  moneyPool += 25;
+}
 
